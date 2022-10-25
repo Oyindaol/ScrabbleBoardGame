be7555a2566fb8f1691ea5b587d49fb7d9b9cb0a
Edidiong <edidiongetetim@gmail.com>
2022-10-25 12:14:20 -0400

    Added Java Docs

--- src/Tile.java
+++ src/Tile.java
@@ -1,7 +1,21 @@
+/**
+ * The Tile class represents a single tile in the scrabble game which holds
+ * one specific character.
+ *
+ * @author Edidiong Okon (101204818)
+ * @version October 25, 2022
+ */
+
 public class Tile {
     public int score;
     private String character;
 
+    /**
+     * Constructor for objects of class Tile.
+     * Creates a new tile with a specific character and value (score).
+     * @param character
+     * @param score
+     */
     public Tile(String character, int score){
         this.score = score;
         this.character = character;
@@ -10,12 +24,19 @@ public class Tile {
 //            this.tileLetters.add(tileLetters.charAt(i));
 //        }
     }
-
-    public int getScore() {
-        return score;
-    }
-
+    /**
+     * Returns a tile's character.
+     * @return the character held by a tile
+     */
     public String getCharacter() {
         return character;
+    }
+
+    /**
+     * Returns the value of a tile.
+     * @return the score/value of a tile
+     */
+    public int getScore() {
+        return score;
     }
 }
