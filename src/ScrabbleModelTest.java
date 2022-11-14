8001f9843a08a76a25108745fb4b078805c7755e
ajayitomisin <82481467+ajayitomisin@users.noreply.github.com>
2022-11-13 23:56:57 -0500

    Update ScrabbleModelTest.java

--- src/ScrabbleModelTest.java
+++ src/ScrabbleModelTest.java
@@ -14,11 +14,15 @@ public class ScrabbleModelTest {
         model.playTile('T', 7, 9);
         model.playTile('E', 7, 10);
         model.endTurn();
+        int actualScore = model.calculateScore(model.getSquare(7,7), 7,7);
+        actualScore += model.calculateScore(model.getSquare(7,8), 7,8);
+        actualScore += model.calculateScore(model.getSquare(7,9), 7,9);
+        actualScore += model.calculateScore(model.getSquare(7,10), 7,10);
         int score = ScrabbleModel.getInstance().calculateScore('C', 7, 7);
         score += ScrabbleModel.getInstance().calculateScore('U', 7, 8);
         score += ScrabbleModel.getInstance().calculateScore('T',7, 9);
         score += ScrabbleModel.getInstance().calculateScore('E', 7, 10);
-        assertEquals(score, model.getPlayerScore(0));
+        assertEquals(score, actualScore);
     }
 
     @Test
