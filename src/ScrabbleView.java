73a03673d54e2414e985e557119e08e55100b931
Tomisin Ajayi <ajayitomisin50@gmail.com>
2022-11-13 23:05:27 -0500

    Added ScrabbleView

--- src/ScrabbleView.java
+++ src/ScrabbleView.java
@@ -1,3 +1,10 @@
+/**
+ * Updates the Scrabble view
+ *
+ * @author Oluwatomisin Ajayi (101189490)
+ * @version November 13, 2022
+ */
 public interface ScrabbleView {
-
+    //void update(ScrabbleModel e);
+    void update(ScrabbleModel model, ScrabbleBoardFrame board, int i, int j, boolean turn);
 }
