a0931c09e730a744f2b07e11bd402aa70e8cf29b
Tomisin Ajayi <ajayitomisin50@gmail.com>
2022-11-19 23:03:48 -0500

    Created TileBag class and refactored ScrabbleModel class.

--- src/ScrabbleModel.java
+++ src/ScrabbleModel.java
@@ -20,8 +20,7 @@ public class ScrabbleModel {
     private final char[][] newBoard;
     private final char[][] currentBoard;
 
-    private final char[] tileBag;
-    private final Map<Character, Integer> tiles;
+    private final TileBag newTileBag;
     private static ArrayList<Character> characters;
 
     private final ArrayList<Player> players;
@@ -82,48 +81,10 @@ public class ScrabbleModel {
             }
         }
 
-        //Characters are removed and placed into players rack
-        //keeps track of the character distribution in the game
-        tileBag = new char[] {'A','A','A','A','A','A','A','A','A','B','B','C','C','D','D',
-                'D','D','E','E','E','E','E','E','E','E','E','E','E','E','F',
-                'F','G','G','G','H','H','I','I','I','I','I','I','I','I','I',
-                'J','K','L','L','L','L','M','M','N','N','N','N','N','N','O',
-                'O','O','O','O','O','O','O','P','P','Q','R','R','R','R','R',
-                'R','S','S','S','S','T','T','T','T','T','T','U','U','U','U',
-                'V','V','W','W','X','Y','Y','Z',' ',' '};
-
-
-        //Mapping each character to their points
-        tiles = new HashMap<>();
-        tiles.put('A', 1);
-        tiles.put('B', 3);
-        tiles.put('C', 3);
-        tiles.put('D', 2);
-        tiles.put('E', 1);
-        tiles.put('F', 4);
-        tiles.put('G', 2);
-        tiles.put('H', 4);
-        tiles.put('I', 1);
-        tiles.put('J', 8);
-        tiles.put('K', 5);
-        tiles.put('L', 1);
-        tiles.put('M', 3);
-        tiles.put('N', 1);
-        tiles.put('O', 1);
-        tiles.put('P', 3);
-        tiles.put('Q', 10);
-        tiles.put('R', 1);
-        tiles.put('S', 1);
-        tiles.put('T', 1);
-        tiles.put('U', 1);
-        tiles.put('V', 4);
-        tiles.put('W', 4);
-        tiles.put('X', 8);
-        tiles.put('Y', 4);
-        tiles.put('Z', 10);
+        newTileBag = new TileBag();
 
         characters = new ArrayList<>();
-        for(char i : tileBag) {
+        for(char i : newTileBag.tileBag) {
             characters.add(i);
         }
 
@@ -320,19 +281,19 @@ public class ScrabbleModel {
         switch (currentBoard[row][col]) {
             case 1: //start of the game and also double word bonus
                 isDoubleWord = true;
-                return tiles.get(Character.toUpperCase(c));
+                return newTileBag.tiles.get(Character.toUpperCase(c));
             case 2:
-                return tiles.get(Character.toUpperCase(c)) * 2;
+                return newTileBag.tiles.get(Character.toUpperCase(c)) * 2;
             case 3:
-                return tiles.get(Character.toUpperCase(c)) * 3;
+                return newTileBag.tiles.get(Character.toUpperCase(c)) * 3;
             case 4:
                 isDoubleWord = true;
-                return tiles.get(Character.toUpperCase(c));
+                return newTileBag.tiles.get(Character.toUpperCase(c));
             case 5:
                 isTripleWord = true;
-                return tiles.get(Character.toUpperCase(c));
+                return newTileBag.tiles.get(Character.toUpperCase(c));
             default:
-                return tiles.get(Character.toUpperCase(c));
+                return newTileBag.tiles.get(Character.toUpperCase(c));
         }
     }
 
