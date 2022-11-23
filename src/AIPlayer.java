cc6dcf840f5a2c400727e104125e340b2076bad9
Oyindamola Taiwo-Olupeka <Olupekaoyinda@gmail.com>
2022-11-22 22:44:22 -0500

    Adding in Javadocs

--- src/AIPlayer.java
+++ src/AIPlayer.java
@@ -1,5 +1,10 @@
 import java.util.ArrayList;
 
+/**
+ * AI player class.
+ * Extends the Player class
+ * Plays the best possible word that returns the highest score based on the tiles already on the board.
+ */
 public class AIPlayer extends Player {
 
 
@@ -18,24 +23,46 @@ public class AIPlayer extends Player {
         playerTurn = false;
     }
 
+    /**
+     * Getter method for player name
+     * @return playerName
+     */
     @Override
     public String getPlayerName() {
         return playerName;
     }
 
+    /**
+     * Getter method for playerScore
+     * @return playerScore
+     */
     @Override
     public int getPlayerScore() {
         return playerScore;
     }
 
+    /**
+     * Getter method for player tiles
+     * @return playertiles
+     */
     public ArrayList<Character> getPlayerTiles() {
         return playerTiles;
     }
 
+    /**
+     * getter method to return played tiles
+     * @return usedTiles
+     */
     public ArrayList<Character> getUsedTiles() {
         return usedTiles;
     }
 
+    /**
+     * Getter method to return tiles on the board so AI is able to make a move
+     * @param word
+     * @param character
+     * @return
+     */
     public int getTileOnBoard(char[] word, char character){
         for (int i = 0; i < word.length; i++){
             if (character == word[i]){
@@ -45,6 +72,11 @@ public class AIPlayer extends Player {
         return -1;
     }
 
+    /**
+     * Getter method to return possible words from a combination of tiles on the rack
+     * @param character
+     * @return possibleWords
+     */
     public ArrayList<String> getPossibleWords(char character){
 
         char[] charac;
@@ -60,6 +92,11 @@ public class AIPlayer extends Player {
         return possibleWords;
     }
 
+    /**
+     * Getter method to return score of possible words from a combination of tiles on the rack
+     * @param word
+     * @return score
+     */
     public int getPossibleWordScore(String word){
         int score = 0;
         for(Character c : word.toCharArray()){
@@ -68,6 +105,12 @@ public class AIPlayer extends Player {
         return score;
     }
 
+    /**
+     * Getter method to return the highest score from possible
+     * words made from a combination of tiles on the rack
+     * @param possibleWords
+     * @return
+     */
     public String getHighestWordScore(ArrayList<String> possibleWords){
         String word = " ";
         int score = 0;
